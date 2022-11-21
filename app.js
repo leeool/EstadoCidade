1c565649cbc41cd334bcdd521df34539cb8ddc7b
leeool <lgonsalez02@gmail.com>
2022-11-21 00:54:52 -0300

    :sparkles: IMPROVE: Log City

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ let logContainer = document.querySelector('[data-js="log-container"]')
 
 const estadoLista = [
   {
-    1: "Curitiba (1)",
-    2: "Cascavel (2)",
-    3: "Maringá (3)"
+    PR1: "Curitiba (1)",
+    PR2: "Cascavel (2)",
+    PR3: "Maringá (3)"
   },
   {
-    1: "Florianópolis (4)",
-    2: "Blumenau (5)",
-    3: "Lages (6)"
+    SC4: "Florianópolis (4)",
+    SC5: "Blumenau (5)",
+    SC6: "Lages (6)"
   },
   {
-    1: "Porto Alegre (7)",
-    2: "Gramado (8)",
-    3: "Caxias do Sul (9)"
+    RS7: "Porto Alegre (7)",
+    RS8: "Gramado (8)",
+    RS9: "Caxias do Sul (9)"
   }
 ]
 
@@ -35,23 +35,26 @@ estadoContainer.addEventListener("input", (event) => {
   cidadeContainer.innerHTML = "<Option value=''>Selecione</Option>"
 
   if (inputValue === "Paraná ('PR')") {
-    for ([_, value] of Object.entries(estadoLista[0])) {
-      cidadeContainer.innerHTML += `<Option value="${value}">${value}</Option>`
+    for ([key, value] of Object.entries(estadoLista[0])) {
+      cidadeContainer.innerHTML += `<Option value="${key}">${value}</Option>`
     }
   } else if (inputValue === "Santa Catarina ('SC')") {
-    for ([_, value] of Object.entries(estadoLista[1])) {
-      cidadeContainer.innerHTML += `<Option value="${value}">${value}</Option>`
+    for ([key, value] of Object.entries(estadoLista[1])) {
+      cidadeContainer.innerHTML += `<Option value="${key}">${value}</Option>`
     }
   } else {
-    for([_, value] of Object.entries(estadoLista[2])){
-      cidadeContainer.innerHTML += `<Option value="${value}">${value}</Option>`
+    for([key, value] of Object.entries(estadoLista[2])){
+      cidadeContainer.innerHTML += `<Option value="${key}">${value}</Option>`
     }
   }
   
   cidadeContainer.addEventListener("input", event => {
-    !event.explicitOriginalTarget.value 
-    ? logContainer.textContent = "..." 
-    : logContainer.textContent = event.explicitOriginalTarget.value.slice(0, -3)
-  })
+    const textValueOption = cidadeContainer.options[cidadeContainer.selectedIndex].text
 
+    textValueOption === "Selecione" 
+    ? logContainer.textContent = "..." 
+    : logContainer.textContent 
+    = `${textValueOption.slice(0, -4)}/${cidadeContainer.value.slice(0, -1)}`
+
+  })
 })
